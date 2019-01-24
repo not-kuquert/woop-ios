7aec146c96e89a51ce72126bf40215bec251f896
Marcus Kuquert <marcus.kuquert@icloud.com>
2019-01-23 22:34:25 -0200

    Initial Commit

---
+++ woop-iosTests/woop_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  woop_iosTests.swift
+//  woop-iosTests
+//
+//  Created by Marcus Vinicius Kuquert on 1/23/19.
+//  Copyright © 2019 kuquert. All rights reserved.
+//
+
+import XCTest
+@testable import woop_ios
+
+class woop_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
