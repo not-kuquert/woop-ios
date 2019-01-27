69b56521d81671e68bc7113896e1403b0b887a1e
Marcus Kuquert <marcus.kuquert@icloud.com>
2019-01-27 16:56:05 -0200

    Add dateFormat test

--- woop-iosTests/woop_iosTests.swift
+++ woop-iosTests/woop_iosTests.swift
@@ -17,10 +17,15 @@ class woop_iosTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testDateFormat() {
+        let dateView = DateView()
+        let formatter = DateFormatter(dateFormat: "dd/MM/yyyy")
+        dateView.date = formatter.date(from: "05/03/1995")
+        
+        XCTAssertEqual(dateView.dayLabel.text, "05")
+        XCTAssertEqual(dateView.monthLabel.text, "MAR")
+        XCTAssertEqual(dateView.yearLabel.text, "1995")
     }
 
     func testPerformanceExample() {
